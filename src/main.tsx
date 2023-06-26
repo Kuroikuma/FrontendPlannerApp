f29d2e7b9254f6af1f18c3776ea28bac1c97321e
ArleysGatica <gatica.arleys16@gmail.com>
2023-06-26 16:59:19 -0600

    dadad

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import { BrowserRouter } from "react-router-dom";
-// import { rootReducer } from "./reducers/rootReducers";
 import { applyMiddleware, compose } from "redux";
 
 
@@ -13,8 +10,6 @@ import { applyMiddleware, compose } from "redux";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 
     <Provider store={store}>
-        <BrowserRouter>
-                <App  />
-        </BrowserRouter>
+        <App  />
     </Provider>
 );
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,26 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { NoMatch } from "./noMatch";
 import { Login } from "../pages/Account/Login/Index";
-
-import routes from "./RouterList";
-import { SigIn } from "../UI/organisms/signin/Index";
 
 
 export const Router = () => {
   return (
-    <Routes>
-      <Route
-        path={routes.Home.path}
-        element={
-          <>Hola mundo</>
-        }
-      />
-      <Route
-        path={routes.Login.path}
-        element={
-          <Login />
-        }
-      />
-    </Routes>
+    <BrowserRouter>
+        <Routes>
+        <Route
+            path={"/"}
+            element={
+            <>Hola mundo</>
+            }
+        />
+        <Route
+            path={"/account/login"}
+            element={
+            <Login />
+            }
+        />
+        </Routes>
+    </BrowserRouter>
   );
 };
