ebf947a8e54d0dc90c19d434fa86a6deaa9f8ce4
ArleysGatica <gatica.arleys16@gmail.com>
2023-06-26 16:10:39 -0600

    agregand los boleanos

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 
 function App() {
     console.log(import.meta.env.VITE_PLANNERAPP_URL);
+    console.log(import.meta.env.PROD);
+    console.log(import.meta.env.DEV);
   return (
     <>
       <div className="init-plannerapp">
